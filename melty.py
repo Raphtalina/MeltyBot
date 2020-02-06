1cf45646fc6025c7829e33350e746b27e4a479ee
Raphtalina <57322348+Raphtalina@users.noreply.github.com>
2020-02-06 13:53:46 +0300

    Update melty.py

--- melty.py
+++ melty.py
@@ -32,9 +32,9 @@ class Bot(discord.Client):
                 title = message.content.split()[1]
                 source = message.content.split()[2]
                 artist = message.content.split()[3]
-                e = discord.Embed()
+                e = discord.Embed(color=0xc28f2c)
                 e.set_author(name=title, icon_url='https://i.imgur.com/irR6EX3.png')
-                e.add_field(name='Artist', value=artist)
+                e.add_field(name='Artist', value=', '.join(artist))
                 e.set_image(url=source)
                 await message.channel.send(embed=e)
 
