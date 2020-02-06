6443ca4af6599ef23bd4a512b297b8f2314d7d6c
Raphtalina <57322348+Raphtalina@users.noreply.github.com>
2020-02-06 13:43:52 +0300

    Update melty.py

--- melty.py
+++ melty.py
@@ -31,7 +31,7 @@ class Bot(discord.Client):
             if message.content.startswith('.lolhentai'):
                 title = message.content.split()[1]
                 source = message.content.split()[2]
-                artist = message.content.split()[2]
+                artist = message.content.split()[3]
                 e = discord.Embed()
                 e.set_author(name=title, url='https://i.imgur.com/irR6EX3.png')
                 e.add_field(name='Artist', value=artist)
