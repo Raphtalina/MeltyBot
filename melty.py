2c6bb19ab639af96a801901bce8134f77cb099be
Raphtalina <57322348+Raphtalina@users.noreply.github.com>
2020-02-14 15:07:18 +0300

    Update melty.py

--- melty.py
+++ melty.py
@@ -27,18 +27,14 @@ class Bot(discord.Client):
                     await message.author.remove_roles(discord.utils.get(message.guild.roles, name='Cavamon'))
                 await message.delete()
 
-        if message.channel.id == 672587798783721522:
-            if message.content.startswith('.lolhentai'):
+        if message.channel.id == 670998063220916234:
+            if message.content.startswith('.upload'):
                 title = message.content.split()[1]
                 source = message.content.split()[2]
-                artist = message.content.split()[3]
-                test = message.content.split()[4]
-                e = discord.Embed(color=0x662d4b)
-                if test == '1':
-                    e.add_field(name='Champion', value=title)
-                elif test == '2':
-                    e.add_field(name='Champions', value=', '.join(list(title.split(','))))
-                e.add_field(name='Artist', value=artist)
+                model = message.content.split()[3]
+                link = message.content.split()[4]
+                e = discord.Embed(title=title)
+                e.add_field(name='Model', value=f'[{model}]({link})')
                 e.set_image(url=source)
                 await message.channel.send(embed=e)
 
